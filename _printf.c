3503b9194a0185430254f065824d288b94a138ee
Dice1011 <u12283593@tuks.co.za>
2023-07-31 14:15:58 -0700

    _printf.c debugged

--- _printf.c
+++ _printf.c
@@ -1,13 +1,10 @@
 #include "main.h"
-#include <stdarg.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <strings.h>
+
 /***
  *_printf -Produces output accordint to given format
  *@format:Given format
  *Return:Printed characters
- */
+i */
 int _printf(const char *format, ...)
 {
 	int print = 0;
@@ -26,5 +23,6 @@ int _printf(const char *format, ...)
 		}
 		format++;
 	}
+	va_end(argc);
 	return (print);
 }
